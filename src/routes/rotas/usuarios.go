2a6bc210dd164b74d44edf14d78a525784262e49
thtomazella <88443010+thtomazella@users.noreply.github.com>
2022-02-11 15:31:52 -0300

    feat(Router): Create Router Init (#6)

---
+++ src/routes/rotas/usuarios.go
@@ -0,0 +1,72 @@
+package rotas
+
+import (
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{ // Cadastro o usuario
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             nil, // controllers.CriarUsuario,
+		RequerAutenticacao: false,
+	},
+	/**
+	{ // Busca Todos os Usuários
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticacao: true,
+	},
+
+	{ // Buscar somente um usuario por ID
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticacao: true,
+	},
+
+	{ //Alterar o usuario por ID
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizandoUsuario,
+		RequerAutenticacao: true,
+	},
+	{ // Deletar o registro por ID
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/seguir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.SeguirUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/parar-de-seguir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.PararDeSeguirUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/seguidores",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarSeguidores,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/seguindo",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarSeguindo,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/atualizar-senha",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.AtualizarSenha,
+		RequerAutenticacao: true,
+	},
+	*/
+}
