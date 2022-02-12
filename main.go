afccdcab58d63648fe341b38b6c90f2cd0238718
thtomazella <88443010+thtomazella@users.noreply.github.com>
2022-02-12 14:15:59 -0300

    feat(database): Config database (#14)

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flight/src/config"
+	router "flight/src/routes"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Teste de abertura")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	config.Carregar()
+	r := router.Gerar()
+
+	fmt.Printf("Listening: %d", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
