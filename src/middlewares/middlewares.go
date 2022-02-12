cb54327c7d39f346fad16829bda5dd13b374d7a1
thtomazella <th.tomazella@gmail.com>
2022-02-12 10:51:55 -0300

    feat(authentucation): Create authentication

---
+++ src/middlewares/middlewares.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"flight/src/authentication"
+	"log"
+	"net/http"
+)
+
+// Logger escreve informações da requisição no terminal
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		proximaFuncao(w, r)
+	}
+}
+
+// Autenticar verifica se o usuário fazendo a requisição está autenticado.
+func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if erro := authentication.ValidarToken(r); erro != nil {
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
+		proximaFuncao(w, r)
+	}
+
+}
