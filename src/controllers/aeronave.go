cf51acd20d811d9fd951c9b689f14a47a72beb25
thtomazella <88443010+thtomazella@users.noreply.github.com>
2022-02-19 09:11:07 -0300

    feat(aircraft) :Created search by prefix and all aircraft (#26)

--- src/controllers/aeronave.go
+++ src/controllers/aeronave.go
@@ -9,9 +9,12 @@ import (
 	"flight/src/response"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-// CriarAeroporto insere um aeroporto no banco de dados
+// CriarAeronave insere uma aeronave no banco de dados
 func CriarAeronave(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
 
@@ -49,7 +52,7 @@ func CriarAeronave(w http.ResponseWriter, r *http.Request) {
 		response.Erro(w, http.StatusBadRequest, errors.New(" PREFIXO já informado! Operação Bloqueada. "))
 		return
 	}
-	
+
 	repositorio := repository.NovoRepositoDeAeronave(db)
 	aeronave.ID, erro = repositorio.Criar(aeronave)
 	if erro != nil {
@@ -58,4 +61,95 @@ func CriarAeronave(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response.JSON(w, http.StatusCreated, aeronave)
+}
+
+// AtualizandoAeronave altera as informacoes de somente uma aeronave
+func AtualizandoAeronave(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	aeronaveID, erro := strconv.ParseUint(parametros["aeronaveid"], 10, 64)
+	if erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if !json.Valid([]byte(corpoRequisicao)) {
+		response.Erro(w, http.StatusBadRequest, errors.New(" Registro enviado para servidor não compativel com o sistema. "))
+		return
+	}
+
+	var aeronave models.Aeronave
+
+	if erro = json.Unmarshal(corpoRequisicao, &aeronave); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = aeronave.Preparar("edicao"); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	bd, erro := database.Conectar()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer bd.Close()
+
+	repositorio := repository.NovoRepositoDeAeronave(bd)
+	if erro = repositorio.Atualizar(aeronaveID, aeronave); erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	response.JSON(w, http.StatusNoContent, nil)
+
+}
+
+// BuscarAeronaves retorna todas as aeronaves
+func BuscarAeronaves(w http.ResponseWriter, r *http.Request) {
+
+	db, erro := database.Conectar()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+	}
+	defer db.Close()
+
+	repositorio := repository.NovoRepositoDeAeronave(db)
+
+	aeronaves, erro := repositorio.BuscarTodos(1)
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	response.JSON(w, http.StatusOK, aeronaves)
+}
+
+// BuscarAeronave retorna a aeronave por prefixo
+func BuscarAeronave(w http.ResponseWriter, r *http.Request) {
+
+	parametros := mux.Vars(r)
+	prefixoAeronave := parametros["prefixo"]
+
+	db, erro := database.Conectar()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+	}
+	defer db.Close()
+
+	repositorio := repository.NovoRepositoDeAeronave(db)
+
+	aeronave, erro := repositorio.BuscarAeronavePorPrefixo(prefixoAeronave)
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	response.JSON(w, http.StatusOK, aeronave)
+
 }
--- src/repository/aeronave.go
+++ src/repository/aeronave.go
@@ -6,17 +6,17 @@ import (
 )
 
 // Aeronave representa um repositorio de aeronaves
-type Aeronave struct {
+type Aeronaves struct {
 	db *sql.DB
 }
 
 // NovoRepositoDeAeronave cria um repositorio de aeronaves
-func NovoRepositoDeAeronave(db *sql.DB) *Aeronave {
-	return &Aeronave{db}
+func NovoRepositoDeAeronave(db *sql.DB) *Aeronaves {
+	return &Aeronaves{db}
 }
 
 // Criar insere uma aeronave no bando de dados
-func (repositorio Aeronave) Criar(aeronave models.Aeronave) (uint64, error) {
+func (repositorio Aeronaves) Criar(aeronave models.Aeronave) (uint64, error) {
 	statement, erro := repositorio.db.Prepare(
 		`INSERT INTO aeronave(nome, prefixo, preco, custo) 
 		VALUES(?, ?, ?, ?)`,
@@ -40,8 +40,34 @@ func (repositorio Aeronave) Criar(aeronave models.Aeronave) (uint64, error) {
 
 }
 
+// Atualizar altera as informações de uma aeronave no banco de dados
+func (repositorio Aeronaves) Atualizar(ID uint64, aeronave models.Aeronave) error {
+	statement, erro := repositorio.db.Prepare(`
+	UPDATE aeronave SET nome= ?, 
+		prefixo = ?,
+		custo = ?,
+		preco = ?
+		WHERE id = ?`)
+
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(
+		aeronave.Nome,
+		aeronave.Prefixo,
+		aeronave.Custo,
+		aeronave.Preco,
+		ID); erro != nil {
+		return erro
+
+	}
+	return nil
+}
+
 //BuscarPorPrefixo para bloqueio de registro duplicado
-func (repositorio Aeronave) BuscarPorPrefixo_Duplicado(_prefixo string) string {
+func (repositorio Aeronaves) BuscarPorPrefixo_Duplicado(_prefixo string) string {
 	linha, erro := repositorio.db.Query(`
 			SELECT id
 			  FROM aeronave where prefixo= ? `, _prefixo)
@@ -55,4 +81,67 @@ func (repositorio Aeronave) BuscarPorPrefixo_Duplicado(_prefixo string) string {
 	} else {
 		return "NO"
 	}
+}
+
+// BuscarTodos retorna todas as aeronaves
+func (repositorio Aeronaves) BuscarTodos(codInt int) ([]models.Aeronave, error) {
+
+	linhas, erro := repositorio.db.Query(`
+	SELECT id, 
+		   nome, 
+		   prefixo,
+		   custo,
+		   preco,
+		   inclusion
+	  FROM aeronave
+	  WHERE id >= ? ORDER BY nome`, codInt)
+
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var aeronaves []models.Aeronave
+
+	for linhas.Next() {
+		var aeronave models.Aeronave
+
+		if erro = linhas.Scan(&aeronave.ID,
+			&aeronave.Nome,
+			&aeronave.Prefixo,
+			&aeronave.Custo,
+			&aeronave.Preco,
+			&aeronave.Inclusion); erro != nil {
+			return nil, erro
+		}
+		aeronaves = append(aeronaves, aeronave)
+	}
+	return aeronaves, nil
+
+}
+
+// BuscarPorANAC retorna os dados de um usuário específico por ID ANAC
+func (repositorio Aeronaves) BuscarAeronavePorPrefixo(_prefixo string) (models.Aeronave, error) {
+	// _descricao = fmt.Sprintf("%%%s%%", _descricao)
+	linha, erro := repositorio.db.Query(`
+			SELECT id, nome, prefixo, custo, preco
+			  FROM aeronave where prefixo = ?`, _prefixo)
+	if erro != nil {
+		return models.Aeronave{}, erro
+	}
+	defer linha.Close()
+
+	var aeronave models.Aeronave
+
+	if linha.Next() {
+		if erro = linha.Scan(&aeronave.ID,
+			&aeronave.Nome,
+			&aeronave.Prefixo,
+			&aeronave.Custo,
+			&aeronave.Preco); erro != nil {
+			return models.Aeronave{}, erro
+		}
+	}
+	return aeronave, nil
+
 }
